8fc535accf9e1dec6913081bc98974fb9448c71c
ceci6182 <ceci6182@easv365.dk>
2022-05-31 14:46:05 +0200

    Person.Name is unique

--- de4aber.emilseBilseBingo.DataAccess/MainDBContext.cs
+++ de4aber.emilseBilseBingo.DataAccess/MainDBContext.cs
@@ -1,3 +1,4 @@
+using de4aber.emilseBilseBingo.Core.Models;
 using de4aber.emilseBilseBingo.DataAcess.Entities;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,5 +13,12 @@ namespace de4aber.emilseBilseBingo.DataAcess
         public virtual DbSet<PersonEntity> Persons { get; set; }
 
         public virtual DbSet<TileItemEntity> TileItemEntities { get; set; }
+        
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<PersonEntity>()
+                .HasIndex(u => u.Name)
+                .IsUnique();
+        }
     }
 }
