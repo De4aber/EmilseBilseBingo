bb3082f3d1c19b4c115fa642883913a79a7cc823
ceci6182 <ceci6182@easv365.dk>
2022-05-31 21:16:27 +0200

    Update TileItemRepository.cs

--- de4aber.emilseBilseBingo.DataAccess/Repositories/TileItemRepository.cs
+++ de4aber.emilseBilseBingo.DataAccess/Repositories/TileItemRepository.cs
@@ -29,46 +29,58 @@ namespace de4aber.emilseBilseBingo.DataAcess.Repositories
                 list.Add(ent);
                 
             }
+            await _connection.CloseAsync();
 
             return list;
         }
 
         public async Task<TileItem> FindById(int id)
         {
+            TileItem ent = null;
             await _connection.OpenAsync();
+            
 
             await using var command = new MySqlCommand($"SELECT * FROM `{_table}` WHERE `id` = {id};", _connection);
             await using var reader = await command.ExecuteReaderAsync();
             while (await reader.ReadAsync())
             {
-                var ent = new TileItem(reader.GetValue(1).ToString(),(int) reader.GetValue(2))
+                ent = new TileItem(reader.GetValue(1).ToString(),(int) reader.GetValue(2))
                 {
                     Id = (int) reader.GetValue(0)
                 };
-                return ent;
-
             }
 
-            throw new InvalidDataException("TileItem with that id does not exist");
+            await _connection.CloseAsync();
+
+            if (ent == null)
+            {
+                throw new InvalidDataException("TileItem with that id does not exist");
+            }
+            return ent;
         }
 
         public async Task<TileItem> Create(TileItem tileItem)
         {
+            TileItem ent = null;
             await _connection.OpenAsync();
 
             await using var command = new MySqlCommand($"INSERT INTO `{_table}`(`bingoCondition`, `ofPersonId`) VALUES ('{tileItem.Condition}', '{tileItem.OfPersonId}'); SELECT * FROM `{_table}` WHERE `id` = LAST_INSERT_ID();", _connection);
             await using var reader = await command.ExecuteReaderAsync();
             while (await reader.ReadAsync())
             {
-                var ent = new TileItem(reader.GetValue(1).ToString(),(int) reader.GetValue(2))
+                ent = new TileItem(reader.GetValue(1).ToString(),(int) reader.GetValue(2))
                 {
                     Id = (int) reader.GetValue(0)
                 };
-                return ent;
-
             }
+            
+            await _connection.CloseAsync();
 
-            throw new InvalidDataException("ERROR: TileItem not created");
+            if (ent == null)
+            {
+                throw new InvalidDataException("ERROR: TileItem not created");
+            }
+            return ent;
         }
     }
 }
